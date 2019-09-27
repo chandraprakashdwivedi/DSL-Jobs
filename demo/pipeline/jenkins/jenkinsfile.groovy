bf6eca68bae0877ef67c49b4cfafcaab8ed132b3
CP <chandra.prakash363@gmail.com>
2019-09-27 14:49:34 +0530

    Update jenkinsfile.groovy

--- demo/pipeline/jenkins/jenkinsfile.groovy
+++ demo/pipeline/jenkins/jenkinsfile.groovy
@@ -1,1 +1,16 @@
-1
+pipeline {
+
+    agent { label 'devl' }  //jenkins slave on which pipeline will deploy
+
+    stages {
+
+        stage('Hello World') {
+            steps {
+                dir('pipeline/scripts') {
+                    sh 'sh ./hello.sh'
+                }
+            }
+        }
+    }
+
+}
